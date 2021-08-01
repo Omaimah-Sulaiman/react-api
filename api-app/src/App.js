ebbf353b262a31a1a788580edfa3630ad0086e87
Omaimah-Sulaiman <omaymahshakhatrah@gmail.com>
2021-08-01 20:32:19 +0300

    solve tickit 2

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationData: {},
+    }
+  }
+
+  submitForm = async (e) => {
+    e.preventDefault();
+    const location = e.target.locationName.value;
+    
+    const response = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_TOKEN}&q=${location}&format=json`);
+
+    console.log('our axios response', response.data[0]);
+    console.log('our axios response', response.data[0].lat);
+    console.log('our axios response', response.data[0].lon);
+    
+    
+    this.setState({
+      locationData: response.data[0]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.submitForm}>
+          <label>
+            Location Name:
+          </label>
+          <input name="locationName" type="text" placeholder="Enter the location name you want to search" />
+          <input type="submit" value="search Location" />
+        </form>
+        <div>
+          <h1>
+            Location information
+          </h1>
+          {
+     
+            this.state.locationData.display_name &&
+            <p>
+              {this.state.locationData.display_name}<br/>Latitude:  {this.state.locationData.lat} <br/> Longitude:{this.state.locationData.lon} 
+            </p>
+             
+            
+          }
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
