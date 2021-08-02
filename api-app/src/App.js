3119369b425cc3c75e1f719538cf127659d5d25f
Omaimah-Sulaiman <omaymahshakhatrah@gmail.com>
2021-08-02 08:21:09 +0300

    solve ticket 4

--- api-app/src/App.js
+++ api-app/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import Header from "./components/header";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'react-bootstrap/Image';
+import './App.css'
 
 export class App extends Component {
 
@@ -9,51 +11,57 @@ export class App extends Component {
     super(props);
     this.state = {
       locationData: {},
-      mapInfo:""
     }
   }
 
   submitForm = async (e) => {
     e.preventDefault();
     const location = e.target.locationName.value;
-    
+    try{
     const response = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_IQ_TOKEN}&q=${location}&format=json`);
-    const response1 = await axios.get(`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_TOKEN}&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=16&size=480x480&markers=icon:large-red-cutout|17.450419,78.381149&markers=icon:large-red-cutout|17.451377,78.379525&path=fillcolor:%23add8e6|weight:1|color:blue|17.452945,78.380055|17.452765,78.382026|17.452020,78.381375|17.452045,78.380846|17.452945,78.380055
-    `);
-
-
     console.log('our axios response', response.data[0]);
     console.log('our axios response', response.data[0].lat);
-    console.log('our axios response', response1.data);
-    
-    
     this.setState({
-      locationData: response.data[0],
-      mapInfo:response1.data
+      locationData: response.data[0]
     });
+    } 
+    catch(err) {
+      alert(err); 
+    }
+    
   };
+  
+  // onClickSubmit=()=>{
+  //   if(!(this.state.locationData)){
+  //     alert('this location not supported on this browser') 
+  //     // this.setState({
+  //     //   locationData:{}
+  //     // })
+  //   }
+  // }
 
   render() {
     return (
       <div>
-        <form onSubmit={this.submitForm}>
+        <Header/>
+        <form onSubmit={this.submitForm} id="form">
           <label>
             Location Name:
           </label>
-          <input name="locationName" type="text" placeholder="Enter the location name you want to search" />
+          <input name="locationName" type="text" placeholder="Enter the location name you want to search" /><br/>
           <input type="submit" value="search Location" />
         </form>
         <div>
           <h1>
             Location information
           </h1>
-          {
-            this.state.locationData.display_name &&
+          { 
+            this.state.locationData.display_name && 
             <p>
-              {this.state.locationData.display_name}<br/>Latitude:{this.state.locationData.lat} <br/> Longitude:{this.state.locationData.lon} 
-              <Image src={this.state.mapInfo} fluid />
+              {this.state.locationData.display_name}<br/>Latitude:{this.state.locationData.lat} <br/> Longitude:{this.state.locationData.lon} <br/>
+              <Image src={`https://maps.locationiq.com/v3/staticmap?key=${this.state.locationData.display_name}&center=${this.state.locationData.lat},${this.state.locationData.lon}&zoom=16&size=480x480&markers=icon:large-red-cutout|${this.state.locationData.lat},${this.state.locationData.lon}`}  />
             </p>
-          }
+           }
         </div>
       </div>
     )
